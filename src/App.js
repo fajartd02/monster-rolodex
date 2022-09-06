6d7c4b671800623deaf09ba9430d1203544503af
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-09-06 14:48:06 +0800

    react using shallow merge in setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: "Yihuan"
+      name: { firstName: "Andre", lastName: "Simanjuntak" },
+      company: "ZTM",
     };
   }
 
@@ -17,12 +18,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button onClick={() => {
-            this.setState({name: 'Andre'})
-          }}
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
+            {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({
+                name: { firstName: "Xiao", lastName: "Vushinghau" },
+                company: "Google",
+              });
+            }}
           >
-            Change Name
+            Change Name and Company
           </button>
         </header>
       </div>
