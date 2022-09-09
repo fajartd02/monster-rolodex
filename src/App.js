79b492c2a9a05ee4b468d6e4e73f7b2261b244f2
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-09-10 00:48:31 +0800

    create card list components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -50,13 +51,15 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+
+        <CardList />
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello i'm CardList!</p>
+      </div>
+    );
+  }
+}
+
+export default CardList;
