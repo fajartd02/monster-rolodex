4ffc13325489ce8bf5a2db998b63a21d621ffee0
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-09-12 00:09:17 +0800

    card components jsx

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,8 +5,11 @@ class Card extends Component {
   render() {
     const { email, name, id } = this.props.monster;
     return (
-      <div className="card-container">
-        <img alt={name} src={`https://robohash.org/${id}?set=set2`} />
+      <div className="card-container" key={id}>
+        <img
+          alt={name}
+          src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        />
         <h2>{name}</h2>
         <p>{email}</p>
       </div>
