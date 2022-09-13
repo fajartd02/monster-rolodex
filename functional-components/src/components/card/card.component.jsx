c95c0797f3e5e6a10d31a4e9f29b49e6d401ad00
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-09-13 18:36:41 +0800

    change to functional components card

--- functional-components/src/components/card/card.component.jsx
+++ functional-components/src/components/card/card.component.jsx
@@ -1,20 +1,17 @@
-import { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { email, name, id } = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={name}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ monster }) => {
+  const { email, name, id } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={name}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
